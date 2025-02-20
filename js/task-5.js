a739a7b6b8c61c64d8089ab67b28fe5a4980c1f0
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-02-21 00:50:23 +0200

    final-1

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+buttonEl.addEventListener('click', handleClick);
+function handleClick(event) {
+  const newColor = getRandomHexColor();
+  spanEl.textContent = `- ${newColor}`;
+  bodyEl.style.backgroundColor = newColor;
+}
